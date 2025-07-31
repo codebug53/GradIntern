cdd0feb508675a934896a23a64a0fd4433667b71
Tadimarri Mahammad Azhar <azhar.23bce9422@vitapstudent.ac.in>
2025-07-31 23:32:00 +0530

    Adding source code fo the project

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('loginBtn').addEventListener('click', () => {
+  console.log('Login clicked – navigate to login page');
+});
+
+document.getElementById('exploreBtn').addEventListener('click', () => {
+  console.log('Explore clicked – navigate onward');
+});
